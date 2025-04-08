d90ad405fb0c00379238bea1d2d4ab959d0597e8
YuNing Chen <admin@ynchen.me>
2025-04-08 11:31:29 +0800

    utils cleanup

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Captures the prefix (including space) for bullet lists, numbered lists
+ * and checklists
+ */
+export const LIST_CHARACTER_REGEX = /^\s*(-|\+|\*|\d+\.|>) (\[.\] )?/;
---
+++ src/utils.ts
@@ -0,0 +1,40 @@
+import { LIST_CHARACTER_REGEX } from "./constants";
+
+/**
+ * Determines the next markdown list character prefix for a given line.
+ *
+ * If it's an ordered list and direction is `after`, the prefix will be
+ * incremented by 1.
+ *
+ * If it's a checklist, the newly inserted checkbox will always be unticked.
+ *
+ * If the current list item is empty, this will be indicated by a `null` prefix.
+ */
+export const getNextListPrefix = (
+	text: string,
+	direction: "below" | "above"
+): string | null => {
+	const listChars = text.match(LIST_CHARACTER_REGEX);
+	if (listChars && listChars.length > 0) {
+		let prefix = listChars[0].trimStart();
+		const isEmptyListItem = prefix === listChars.input?.trimStart();
+		if (isEmptyListItem) {
+			return null;
+		}
+		if (isNumeric(prefix) && direction === "above") {
+			prefix = +prefix + 1 + ". ";
+		}
+		if (prefix.startsWith("- [") && !prefix.includes("[ ]")) {
+			prefix = "- [ ] ";
+		}
+		return prefix;
+	}
+	return "";
+};
+
+/**
+ * Checks if an input string is numeric.
+ *
+ * Adapted from https://stackoverflow.com/a/60548119
+ */
+export const isNumeric = (input: string) => input.length > 0 && !isNaN(+input);
