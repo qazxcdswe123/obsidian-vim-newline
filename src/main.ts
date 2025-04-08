49d8d7f96ebc746cccb393b5a7844f8b794f548d
YuNing Chen <admin@ynchen.me>
2025-04-08 12:14:15 +0800

    poc

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,99 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from "obsidian";
+
+import { getNextListPrefix } from "./utils";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface UserSettings {
 	autoInsertListPrefix: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: UserSettings = {
 	autoInsertListPrefix: true,
 };
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SmartNewlinePlugin extends Plugin {
+	settings: UserSettings;
 
 	async onload() {
 		await this.loadSettings();
+
+		this.setCommands();
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
-	onunload() {}
+	/**
+	 * Setup the command to handle new lines
+	 */
+	setCommands() {
+		this.addCommand({
+			id: "insertLineBelow",
+			name: "Insert Line Below",
+			editorCallback: (editor, _) => {
+				if (this.settings.autoInsertListPrefix) {
+					this.handleNewLine("below");
+				} else {
+					editor.replaceRange("\n", editor.getCursor());
+					editor.setCursor({
+						line: editor.getCursor().line + 1,
+						ch: 0,
+					});
+				}
+			},
+		});
+		this.addCommand({
+			id: "insertLineAbove",
+			name: "Insert Line Above",
+			editorCallback: (editor, _) => {
+				if (this.settings.autoInsertListPrefix) {
+					this.handleNewLine("above");
+				} else {
+					editor.replaceRange("\n", editor.getCursor());
+					editor.setCursor({
+						line: editor.getCursor().line - 1,
+						ch: 0,
+					});
+				}
+			},
+		});
+	}
+
+	/**
+	 * Handles creating a new line with context-aware list prefixes
+	 */
+	handleNewLine(direction: "below" | "above") {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!view) return;
+
+		const editor = view.editor;
+		const cursor = editor.getCursor();
+		const line = editor.getLine(cursor.line);
+
+		// Get the appropriate list prefix based on the current line
+		const prefix = getNextListPrefix(line, direction);
+
+		// Create new line and insert the proper list prefix if needed
+		if (direction === "below") {
+			// Default Vim 'o' behavior: create a new line below
+			editor.replaceRange("\n", { line: cursor.line, ch: line.length });
+			editor.setCursor({ line: cursor.line + 1, ch: 0 });
+		} else {
+			// Default Vim 'O' behavior: create a new line above
+			editor.replaceRange("\n", { line: cursor.line, ch: 0 });
+			editor.setCursor({ line: cursor.line, ch: 0 });
+		}
+
+		// Insert the list prefix if it exists and is not null
+		if (prefix !== null) {
+			editor.replaceRange(prefix, editor.getCursor());
+			editor.setCursor({
+				line: editor.getCursor().line,
+				ch: prefix.length,
+			});
+		}
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -44,26 +108,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
 class SettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: SmartNewlinePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SmartNewlinePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
